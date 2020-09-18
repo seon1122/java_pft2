5ff0eeea63457ae5b5ccc2cf9b43be2c0d1fa647
Vladyslav Korol <vladyslavkorol@Vladyslavs-MBP.fios-router.home>
2020-09-17 23:49:59 -0400

    Added HelperBase class so that helper classes can interact with each other

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -1,7 +1,5 @@
 package ru.stqa.pft.addressbook.appmanager;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.NoAlertPresentException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -26,15 +24,6 @@ public class ApplicationManager {
 
     public void stop() {
         wd.quit();
-    }
-
-    public boolean isAlertPresent() {
-      try {
-        wd.switchTo().alert();
-        return true;
-      } catch (NoAlertPresentException e) {
-        return false;
-      }
     }
 
     public GroupHelper getGroupHelper() {
